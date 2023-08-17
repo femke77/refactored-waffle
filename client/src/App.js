b7aac6f1d1b40f39adece93b95ce06f7d8bd21be
femke77 <megan.meyers.388@gmail.com>
2023-08-16 20:01:45 -0700

    minor fix to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import { ApolloProvider } from "@apollo/client";
 import { client, Chat } from "./Chat";
-import ChannelsList from "./ChannelsList";
+
 function App() {
   return (
     <ApolloProvider client={client}>
       <div className="App">
         <h2>Welcome to Apollo Chat 💭</h2>
         <Chat />
-        <ChannelsList />
+   
       </div>
     </ApolloProvider>
   );
